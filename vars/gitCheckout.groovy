2c704e70ba6deeb7d46f319fd5155b87f648761b
Krishan Kumar <krishankumarr98@gmail.com>
2023-02-05 12:08:36 +0530

    Ad

--- vars/gitCheckout.groovy
+++
@@ -1,9 +0,0 @@
-def gitCheckout(repoUrl,branch="master"){
-    checkout([
-        $class: 'GitScm'
-        branches: [[name: "*/$branch"]],
-        userRemoteConfigs:[[
-            url: repoUrl
-        ]]
-    ])
-}
